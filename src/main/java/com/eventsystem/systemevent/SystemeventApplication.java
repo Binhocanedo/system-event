40e0ecb3e1302db49f832714edfd697e7de68705
FÁBIO CANEDO LEÃO <236462@facens.br>
2024-04-12 09:39:32 -0300

    inital commit

---
+++ src/main/java/com/eventsystem/systemevent/SystemeventApplication.java
@@ -0,0 +1,13 @@
+package com.eventsystem.systemevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemeventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventsystem/systemevent/SystemeventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventsystem.systemevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
